78f84aa05ebc8be5dd5c14352e5ac8f0c7412908
Ivan Puzek <ivan.puzek@gmail.com>
2015-10-26 18:57:27 +0100

    početak

---
+++ Coursera/best.R
@@ -0,0 +1,21 @@
+
+### ASSIGNMENT 3 ###
+
+setwd("~/R/assignment_3/")
+
+best <- function(state, outcome) {
+  
+  ## Read outcome data
+  
+  outcome.data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  
+  
+  ## Check that state and outcome are valid
+  
+  ## Return hospital name in that state with lowest 30-day death
+  ## rate
+}
+
+
+# swirl - looking at data
+str(plants)
