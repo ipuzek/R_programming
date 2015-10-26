049d2b1fa5699834ad8443d99201877d6ce51832
Ivan Puzek <ivan.puzek@gmail.com>
2015-10-26 21:18:35 +0100

    svi elementi rade - posložiti

--- Coursera/best.R
+++ Coursera/best.R
@@ -11,20 +11,52 @@ best <- function(state, outcome) {
   
   #koje varijable? 30 day mortality
   
-  #rename varijabli#
+  #rename varijabli
   names(outcome.data)[11] <- "heart attack"
   names(outcome.data)[17] <- "heart failure"
   names(outcome.data)[23] <- "pneumonia"
   
+  #objekt za check imena
+  valid.names <- c("heart attack","heart failure", "pneumonia")
+  
   ## Check that state and outcome are valid
   
+  #checking state
+  outcome.data$State <- as.factor(outcome.data$State)
+  if (!is.element(state, unique(outcome.data$State))){
+    stop("invalid state")
+  }
   
+  #checking outcome
+  if (!is.element(outcome, valid.names)){
+    stop("invalid outcome")
+  }
   
   ## Return hospital name in that state with lowest 30-day death
   ## rate
+  
+  
+  
 }
+
+
+!is.element("AL", unique(outcome.data$State))
+min(outcome.data$"heart attack")
+
+
+
+outcome.data.state <- subset(outcome.data, subset = outcome.data$State=="AK")
 
 
 # swirl - looking at data
 names(outcome.data)
 str(outcome.data)
+
+
+ods.ordered <- outcome.data.state[order(outcome.data.state$'heart attack', outcome.data.state$Hospital.Name),]
+
+
+# BTINO! newdata <- mtcars[order(mpg, -cyl),]
+
+class(ods.ordered)
+
