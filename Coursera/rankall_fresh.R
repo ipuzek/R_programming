9bc6cad2ecb7293ff930deecb77bb42179a575d5
Ivan Puzek <ivan.puzek@gmail.com>
2015-10-27 23:00:25 +0100

    pri kraju - testirati

--- Coursera/rankall_fresh.R
+++ Coursera/rankall_fresh.R
@@ -1,64 +1,95 @@
 setwd("R/assignment_3/")
 
-dat <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-
-
-dat[,11] <- as.numeric(dat[,11])
-dat[,17] <- as.numeric(dat[,17])
-dat[,23] <- as.numeric(dat[,23])
-
-dat$State <- as.factor(dat$State)
-
-#rename varijabli
-names(dat)[11] <- "heartattack"
-names(dat)[17] <- "heartfailure"
-names(dat)[23] <- "pneumonia"
-
-
-dat.rank <- aggregate(dat$heartattack ~ dat$State, dat, rank, na.action = na.omit, simplify = TRUE)
-
-names(dat.rank)[2] <- "HAlist"
-
-ke <- dat.rank$HAlist
-attributes(ke) <- NULL
-
-znj <- data.frame(t(sapply(ke,c)))
-#str(znj)
-
-empty <- matrix(nrow=227, ncol=1)
-
-for (i in 1:54) {
-  vec <- unlist(znj[,i], use.names=FALSE)
-  length(vec) <- 227
-  
-  empty <- cbind(empty,vec)
-}
-
-full <- empty[,-1]
-
-summary(empty)
-
-attach(dat.rank)
-mylist <- as.data.frame(dat$`heart attack`)
-head(mylist)
-
 rankall <- function(outcome, num = "best") {
   
   ## Read outcome data
   
+  master <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  master <- subset(master, select=colnames(master)[c(2,7,11,17,23)])
+  names(master)[c(3,4,5)] <- c("HA","HF","Pne")
+  
+  master$HA <- as.numeric(master$HA) 
+  master$HF <- as.numeric(master$HF) 
+  master$Pne <- as.numeric(master$Pne)
+  
+  master$State <- as.factor(master$State)
+  
+  master.subs <- subset(master, select=colnames(master)[c(1,2,3,4,5)], na.rm = TRUE)
   
   
   ## Check that state and outcome are valid
+
+  #objekt za check imena
+  valid.names <- c("heart attack","heart failure", "pneumonia")
   
+  #checking number # možda ni ne treba formalni check, vidjet ćemo
+  
+  
+  #formatiram number
+  if (num=="best"){
+    num <- 1
+  } else if (num=="worst") {
+    num <- 1
+    numworst <- 1
+  } else {
+    num <- num
+  }
+  
+  
+  #checking outcome
+  if (!is.element(outcome, valid.names)){
+    stop("invalid outcome")
+  }
+
+  #formatiram outcome  
+  if (outcome=="heart attack"){
+    outcome <- "HA"
+  } else if (num=="heart failure") {
+    outcome <- "HF"
+  } else {
+    outcome <- "Pne"
+  }
   
   ## For each state, find the hospital of the given rank
-  
+
+  if (numworst==1){
+  master.ord <- master.subs[-order(master.subs[[outcome]], master.subs$Hospital.Name),]  
+  } else {
+  master.ord <- master.subs[order(master.subs[[outcome]], master.subs$Hospital.Name),]
+  }
   
   ## Return a data frame with the hospital names and the
   ## (abbreviated) state name
   
+  ### SPLIT ###
   
+  master.ord.split <- split(master.ord, f = master.ord$State, drop = TRUE)
+  
+  lap <- lapply(master.ord.split, function(x) x[num,1])
+  lap.df <- as.data.frame(unlist(lap))
+  
+  #zadnji tweak konačnog frejma
+  
+  lap.df$state <- rownames(lap.df)
+  names(lap.df) <- c("hospital","state")
+  
+  print(lap.df)
 }
+
+
+
+
+
+#sortiram prema outcomeu pa prema imenu ## u funkciji zamijeniti outcome!!
+
+
+
+#master.rank <- aggregate(master$HA ~ master$State, master, rank, na.action = na.omit, simplify = TRUE)
+
+
+
+
+
 
 ## S3 method for class 'data.frame'
 aggregate(x, by, FUN, ..., simplify = TRUE)
